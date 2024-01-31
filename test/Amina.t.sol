2a672c805e84d52ebaf38e1cdc01b4ddcdf4d32e
partychad <partychad@protonmail.com>
2024-01-31 21:48:33 +0100

    Sloppy tests added but failing

--- test/Amina.t.sol
+++ test/Amina.t.sol
@@ -5,19 +5,46 @@ import {Test, console} from "forge-std/Test.sol";
 import {Amina} from "../src/Amina.sol";
 
 contract AminaTest is Test {
-    Amina public amina_token;
+    Amina amina;
+    address recipient = address(0x123);
+    address taxRecipient = address(0x456);
 
     function setUp() public {
-        amina_token = new Amina("AMINA", "AMN", 10000000000000000000000000 ether, address(0x1ef58E6dE15e03b4C3B31C49af426E3328c5aEA5));
+        amina = new Amina("AminaToken", "AMN", 1000000, taxRecipient);
     }
 
-    function test_Increment() public {
-        amina_token.mint(address(this), 10000000000000000000000000 ether);
-        amina_token.transfer(address(0x1ef58E6dE15e03b4C3B31C49af426E3328c5aEA5), 1 ether);
-
+    function testMint() public {
+        uint256 mintAmount = 1000;
+        amina.mint(recipient, mintAmount);
+        assertEq(amina.balanceOf(recipient), mintAmount);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-       
+    function testBurn() public {
+        uint256 mintAmount = 1000;
+        uint256 burnAmount = 500;
+        amina.mint(recipient, mintAmount);
+        amina.burn(recipient, burnAmount);
+        assertEq(amina.balanceOf(recipient), mintAmount - burnAmount);
     }
+
+    function testTaxOnTransfer() public {
+        uint256 mintAmount = 1000;
+        uint256 expectedTax = mintAmount * amina.taxPercentage() / 100;
+        amina.mint(recipient, mintAmount);
+        amina.transfer(taxRecipient, mintAmount);
+        assertEq(amina.balanceOf(taxRecipient), expectedTax);
+    }
+
+    function testChangeTaxRecipient() public {
+        address newTaxRecipient = address(0x789);
+        amina.changeTaxRecipient(newTaxRecipient);
+        assertEq(amina.taxAddress(), newTaxRecipient);
+    }
+
+    function testChangeTaxPercentage() public {
+        uint256 newTaxPercentage = 5;
+        amina.changeTaxPercentage(newTaxPercentage);
+        assertEq(amina.taxPercentage(), newTaxPercentage);
+    }
+
 }
