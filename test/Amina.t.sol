b047fd390ba9529cb90ca4f92a9c23fafe18fbea
partychad <partychad@protonmail.com>
2024-02-01 10:14:34 +0100

    :sparkles: Added new address variable, updated mint and burn functions, and modified transfer test case in Amina.t.sol.

--- test/Amina.t.sol
+++ test/Amina.t.sol
@@ -8,6 +8,7 @@ contract AminaTest is Test {
     Amina amina;
     address recipient = address(0x123);
     address taxRecipient = address(0x456);
+    address randomAddr = address(0x111);
 
     function setUp() public {
         amina = new Amina("AminaToken", "AMN", 1000000, taxRecipient);
@@ -16,23 +17,27 @@ contract AminaTest is Test {
     function testMint() public {
         uint256 mintAmount = 1000;
         amina.mint(recipient, mintAmount);
-        assertEq(amina.balanceOf(recipient), mintAmount);
+        assertEq(amina.balanceOf(recipient), mintAmount - mintAmount * amina.taxPercentage() / 100);
     }
 
     function testBurn() public {
         uint256 mintAmount = 1000;
         uint256 burnAmount = 500;
+        uint256 expectedTax = mintAmount * amina.taxPercentage() / 100;
+
         amina.mint(recipient, mintAmount);
         amina.burn(recipient, burnAmount);
-        assertEq(amina.balanceOf(recipient), mintAmount - burnAmount);
+        assertEq(amina.balanceOf(recipient), mintAmount- expectedTax - burnAmount);
     }
 
     function testTaxOnTransfer() public {
         uint256 mintAmount = 1000;
         uint256 expectedTax = mintAmount * amina.taxPercentage() / 100;
         amina.mint(recipient, mintAmount);
-        amina.transfer(taxRecipient, mintAmount);
-        assertEq(amina.balanceOf(taxRecipient), expectedTax);
+        vm.startPrank(recipient);
+        amina.transfer(randomAddr, amina.balanceOf(recipient));
+        uint256 newBalance = mintAmount - expectedTax - ((mintAmount - expectedTax) * amina.taxPercentage() / 100);
+        assertEq(amina.balanceOf(randomAddr), newBalance);
     }
 
     function testChangeTaxRecipient() public {
